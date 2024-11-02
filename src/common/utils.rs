96fa7f13e7a95a148e1a88bc91e00679d19a538b
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-02 23:07:43 +0000

    Set up initial project structure.

--- src/common/utils.rs
+++ src/common/utils.rs
@@ -3,7 +3,7 @@ use std::{
   io::{self, BufRead, BufReader, Lines},
 };
 
-pub fn get_file_lines(file_path: &String) -> Lines<BufReader<File>> {
+pub fn get_file_lines(file_path: String) -> Lines<BufReader<File>> {
   return match File::open(file_path) {
     Err(why) => panic!("{:?}", why),
     Ok(file) => io::BufReader::new(file).lines(),
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::io::BufRead;
-use std::{env, fs::File, io};
+use std::env;
 
 use common::utils::get_file_lines;
 use days::{day_01::day_01, day_02::day_02};
@@ -10,13 +9,14 @@ mod days;
 fn main() {
   let args: Vec<String> = env::args().collect();
   let day = &args[1];
-  let file_path = &args[2];
+  // let file_path = &args[2];
+  let file_path = format!("src/days/day_{}_data.txt", day);
 
   let lines_buffer = get_file_lines(file_path);
 
   match (*day).as_str() {
-    "1" => day_01(lines_buffer),
-    "2" => day_02(lines_buffer),
+    "01" => day_01(lines_buffer),
+    "02" => day_02(lines_buffer),
     _ => println!("Cannot find a module for day {}", day),
   }
 }
