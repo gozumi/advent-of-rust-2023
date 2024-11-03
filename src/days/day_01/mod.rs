7deca83196ed7a2e5bb9b233d68b5e357fdc148a
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-03 13:32:35 +0000

    Implement the solution for day 1.

--- src/days/day_01/mod.rs
+++ src/days/day_01/mod.rs
@@ -3,11 +3,20 @@ use std::{
   io::{BufReader, Lines},
 };
 
+use utils::get_calibration_value;
+
+mod utils;
+
 pub fn execute_day_01(lines_buffer: Lines<BufReader<File>>) {
-  for line in lines_buffer {
-    match line {
-      Ok(bar) => println!("{}", bar),
+  let mut accumulator: u32 = 0;
+  for line_buffer in lines_buffer {
+    match line_buffer {
+      Ok(line) => {
+        accumulator += get_calibration_value(&line[..]);
+      }
       Err(why) => panic!("{:#?}", why),
     };
   }
+
+  println!("{}", accumulator);
 }
