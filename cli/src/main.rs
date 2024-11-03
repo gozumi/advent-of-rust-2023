6a733260cc2fa1d9c520e9e0d131f582b6aa04de
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-03 17:36:07 +0000

    Implement solution for day 1 part 2.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,17 +1,26 @@
 use std::env;
 
 use common::utils::get_file_lines;
-use days::{day_01::execute_day_01, day_02::execute_day_02};
+use days::{
+  day_01::{execute_day_01, execute_day_01_part_02},
+  day_02::execute_day_02,
+};
 
 mod common;
 mod days;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  let day = match &args[1].parse::<u32>() {
-    Ok(d) => *d,
-    Err(_) => panic!("The day argument must be a number!"),
+
+  let params: Vec<&str> = args[1].split(".").collect();
+
+  let day = *params.get(0).unwrap();
+
+  let day = match day.parse::<u32>() {
+    Ok(d) => d,
+    Err(_) => panic!("The `day` argument must be a number!"),
   };
+
   let day = match day < 10 {
     true => format!("0{}", day),
     false => format!("{}", day),
@@ -21,9 +30,23 @@ fn main() {
 
   let lines_buffer = get_file_lines(file_path);
 
-  match &day[..] {
+  let day_and_part = if params.len() > 1 {
+    let part = *params.get(1).unwrap();
+
+    let part = match part.parse::<u32>() {
+      Ok(d) => d,
+      Err(_) => panic!("The `part` argument must be a number!"),
+    };
+
+    format!("{}_part_{}", day, part)
+  } else {
+    format!("{}", day)
+  };
+
+  match &day_and_part[..] {
     "01" => execute_day_01(lines_buffer),
+    "01_part_2" => execute_day_01_part_02(lines_buffer),
     "02" => execute_day_02(lines_buffer),
-    _ => println!("Cannot find a module for day {}", day),
+    _ => println!("Cannot find a module for day {}", day_and_part),
   }
 }
