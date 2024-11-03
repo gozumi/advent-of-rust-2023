7cd9ec2fdb764bf9b42f127527b64e59acf8ae3d
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-03 13:13:36 +0000

    Allow for single digit arguments.

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,20 @@ mod days;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  let day = &args[1];
+  let day = match &args[1].parse::<u32>() {
+    Ok(d) => *d,
+    Err(_) => panic!("The day argument must be a number!"),
+  };
+  let day = match day < 10 {
+    true => format!("0{}", day),
+    false => format!("{}", day),
+  };
+
   let file_path = format!("src/days/day_{0}/data.txt", day);
 
   let lines_buffer = get_file_lines(file_path);
 
-  match (*day).as_str() {
+  match &day[..] {
     "01" => execute_day_01(lines_buffer),
     "02" => execute_day_02(lines_buffer),
     _ => println!("Cannot find a module for day {}", day),
