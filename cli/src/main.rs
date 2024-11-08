3992eccbb1cfd18ce1ed7580f02c6ce7327e6512
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-08 21:58:57 +0000

    Refactor core functional to a separate workspace.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,13 +1,12 @@
 use std::env;
 
 use common::utils::get_file_lines;
-use days::{
+use lib::days::{
   day_01::{execute_day_01, execute_day_01_part_02},
   day_02::execute_day_02,
 };
 
 mod common;
-mod days;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -26,9 +25,9 @@ fn main() {
     false => format!("{}", day),
   };
 
-  let file_path = format!("cli/src/days/day_{0}/data.txt", day);
+  let file_path = format!("lib/src/days/day_{0}/data.txt", day);
 
-  let lines_buffer = get_file_lines(file_path);
+  let lines = get_file_lines(file_path).filter_map(Result::ok).collect();
 
   let day_and_part = if params.len() > 1 {
     let part = *params.get(1).unwrap();
@@ -43,10 +42,12 @@ fn main() {
     format!("{}", day)
   };
 
-  match &day_and_part[..] {
-    "01" => execute_day_01(lines_buffer),
-    "01_part_2" => execute_day_01_part_02(lines_buffer),
-    "02" => execute_day_02(lines_buffer),
-    _ => println!("Cannot find a module for day {}", day_and_part),
-  }
+  let result = match &day_and_part[..] {
+    "01" => execute_day_01(lines),
+    "01_part_2" => execute_day_01_part_02(lines),
+    "02" => execute_day_02(lines),
+    _ => panic!("Cannot find a module for day {}", day_and_part),
+  };
+
+  println!("result for day {} is => {}", day_and_part, result);
 }
