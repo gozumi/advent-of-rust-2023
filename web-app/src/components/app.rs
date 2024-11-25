f92e0c38c952be8367a6bc7f5a3bdba9223183e6
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-25 00:51:20 +0000

    Add properties to the data_input component.

--- web-app/src/components/app.rs
+++ web-app/src/components/app.rs
@@ -1,17 +1,20 @@
 use super::answer::Answer;
 use super::data_input::DataInput;
 use super::header::Header;
-use leptos::{component, view, IntoView};
+use leptos::{component, logging::log, view, IntoView};
 
 stylance::import_style!(styles, "app.module.css");
 
 #[component]
 pub fn App() -> impl IntoView {
+  let on_data_change = Box::new(|value| log!("Value changed: {}", value));
+
   view! {
     <section class=styles::app>
       <Header />
-      <DataInput />
+      <DataInput on_change=on_data_change />
       <Answer />
     </section>
+    n
   }
 }
