e29169833ee92432df48764d0303d9ed540f7abc
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-11-25 00:51:20 +0000

    Add properties to the data_input component.

--- web-app/src/components/data_input.rs
+++ web-app/src/components/data_input.rs
@@ -1,10 +1,25 @@
-use leptos::{component, view, IntoView};
+use leptos::*;
+use wasm_bindgen::JsCast;
 
 #[component]
-pub fn DataInput() -> impl IntoView {
+pub fn DataInput(on_change: Box<dyn Fn(String)>) -> impl IntoView {
+  let (value, setValue) = create_signal(String::new());
+
+  let handle_change = move |ev: web_sys::Event| {
+    if let Some(input) = ev.target().and_then(|t| t.dyn_into::<web_sys::HtmlTextAreaElement>().ok())
+    {
+      let new_value = input.value();
+      setValue(new_value.clone());
+      on_change(new_value);
+    }
+  };
+
   view! {
-    <div class="data-input">
-      <textarea class="data-input__text" />
-    </div>
+    <textarea
+      class="data-input"
+      value=value()
+      on:input=handle_change
+      placeholder="Enter the data here..."
+    />
   }
 }
